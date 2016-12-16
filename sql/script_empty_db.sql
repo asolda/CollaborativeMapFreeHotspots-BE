5dd89b2f96538ffb0006e1ede8a1dfbaac787249
Finalgalaxy <finalgalaxy@gmail.com>
2016-12-16 20:38:17 +0100

    Totally refactored script_empty_db.sql and fixed foreign keys

--- sql/script_empty_db.sql
+++ sql/script_empty_db.sql
@@ -1,34 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Creato il: Dic 16, 2016 alle 10:59
--- Versione del server: 5.7.11
--- Versione PHP: 5.6.19
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: gopher_main
---
+-- Creazione DB (IMPORTANTE: cambiare gopher_main in gopher_test1 ecc. per testare su un DB diverso!)
 DROP DATABASE IF EXISTS gopher_main;
 CREATE DATABASE gopher_main DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE gopher_main;
 
--- --------------------------------------------------------
 
---
--- Struttura della tabella rete_wifi
---
 
+
+-- Creazione tabella rete WiFi
 DROP TABLE IF EXISTS rete_wifi;
 CREATE TABLE rete_wifi (
   id int(11) NOT NULL,
@@ -45,12 +26,7 @@ CREATE TABLE rete_wifi (
   utente int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella segnalazione
---
-
+-- Creazione tabella segnalazione
 DROP TABLE IF EXISTS segnalazione;
 CREATE TABLE segnalazione (
   utente int(11) NOT NULL,
@@ -60,12 +36,7 @@ CREATE TABLE segnalazione (
   visualizzato tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella sessione
---
-
+-- Creazione tabella sessioni
 DROP TABLE IF EXISTS sessione;
 CREATE TABLE sessione (
   session_id char(32) NOT NULL,
@@ -75,12 +46,7 @@ CREATE TABLE sessione (
   utente int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella utente
---
-
+-- Creazione tabella utenti
 DROP TABLE IF EXISTS utente;
 CREATE TABLE utente (
   id int(11) NOT NULL,
@@ -88,63 +54,53 @@ CREATE TABLE utente (
   password varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indici per le tabelle scaricate
---
 
---
--- Indici per le tabelle rete_wifi
---
+
+
+
+
+-- Chiave primaria per rete_WiFi
 ALTER TABLE rete_wifi
-  ADD PRIMARY KEY (id),
-  ADD KEY FK_UTENTE (utente);
+  ADD PRIMARY KEY (id);
 
---
--- Indici per le tabelle segnalazione
---
+-- Chiavi primarie per segnalazione
 ALTER TABLE segnalazione
   ADD PRIMARY KEY (utente,rete_wifi),
-  ADD UNIQUE KEY FK_utente1 (utente),
-  ADD UNIQUE KEY FK_rete_wifi (rete_wifi);
+  ADD UNIQUE KEY FK_segnalazione_RETE_WIFI (utente),
+  ADD UNIQUE KEY FK_segnalazione_UTENTE (rete_wifi);
 
---
--- Indici per le tabelle sessione
---
+-- Chiave primaria per la tabella sessione
 ALTER TABLE sessione
-  ADD PRIMARY KEY (session_id),
-  ADD KEY FK_utente2 (utente);
+  ADD PRIMARY KEY (session_id);
 
---
--- Indici per le tabelle utente
---
+-- Chiave primaria per la tabella utente
 ALTER TABLE utente
   ADD PRIMARY KEY (id);
 
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella rete_wifi
---
+  
+  
+  
+  
+  
+-- Settaggio degli AUTO_INCREMENT
 ALTER TABLE rete_wifi
   MODIFY id int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT per la tabella utente
---
+
 ALTER TABLE utente
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- Limiti per le tabelle scaricate
---
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+  
+  
+  
 
---
--- Limiti per la tabella segnalazione
---
+  
+
+-- Vincoli relazionali
+ALTER TABLE rete_wifi
+  ADD CONSTRAINT FK_rete_wifi_UTENTE FOREIGN KEY (utente) REFERENCES utente(id); 
+  
 ALTER TABLE segnalazione
-  ADD CONSTRAINT FK_rete_wifi FOREIGN KEY (rete_wifi) REFERENCES rete_wifi (id),
-  ADD CONSTRAINT FK_utente1 FOREIGN KEY (utente) REFERENCES utente (id);
+  ADD CONSTRAINT FK_segnalazione_RETE_WIFI FOREIGN KEY (rete_wifi) REFERENCES rete_wifi(id),
+  ADD CONSTRAINT FK_segnalazione_UTENTE FOREIGN KEY (utente) REFERENCES utente(id);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE sessione
+  ADD CONSTRAINT FK_sessione_UTENTE FOREIGN KEY (utente) REFERENCES utente(id);
