7ea122a950b1a5e36849c8b91720845a79789f6b
Finalgalaxy <finalgalaxy@gmail.com>
2016-12-17 13:37:48 +0100

    Added first version of getlistpin

--- node_backend/models/pin.js
+++ node_backend/models/pin.js
@@ -1,21 +1,23 @@
 var connection = require('../connection');
 
 
-function Pin() {
-
-  this.testpoint = function(req, res) {
-    connection.acquire(function(err, con) {
+function Pin(){
+	// Another test to delete in future; retrieves back the payload parameters to check if requests and responses are sent correctly
+	this.testpoint = function(req, res){
+		connection.acquire(function(err, con){
 		res.send({status: 0, message: 'All OK', latitude: req.body.latitude, longitude: req.body.longitude});
     });
   };
   
-  this.getlistpin = function(req, res) {
-	  connection.acquire(function(err, con) {
-		con.query('SELECT * FROM pin WHERE latitude >= 50-100 && latitude <= 50+100 && longitude >= 50-100 && longitude <= 50+100',
-			  function(err, result) {
-				con.release();
-				res.send(JSON.stringify(result));
-			  });
+	this.getlistpin = function(req, res){
+		connection.acquire(function(err, con){
+			con.query('SELECT * FROM rete_wifi WHERE latitudine >= '+req.body.latitude+'-100 && '+
+						'latitudine <= '+req.body.latitude+'+100 && longitudine >= '+req.body.longitude+'-100 && longitudine <= '+req.body.longitude+'+100',
+				function(err, result) {
+					con.release();
+					res.send(JSON.stringify(result));
+				}
+			);
 		});
 		//res.send({status: 0, message: 'All OK', latitude: req.body.latitude, longitude: req.body.longitude});
   };
