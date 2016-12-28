0dc43537d13198bdadf0061479691c71cf3bebd9
matioski <mattia.rosselli@gmail.com>
2016-12-28 22:57:24 +0100

    Aggiunto modulo chai-http nel file JSON e aggiunta cartela di test con test pin (eseguire npm test per testare

---
+++ test/pin.js
@@ -0,0 +1,37 @@
+
+
+let connection = require("../connection");
+let Pin = require('../models/pin');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Pins', () => {
+
+    /*
+      * Test the /GET route
+      */
+    describe('/GET Pin', () => {
+        it('it should GET PINS list', (done) => {
+            it('it should GET PINS list', (done) => {
+                chai.request('http://localhost:8080')
+                    .get('/pin/get_networks/1/2/3/4')
+
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        done();
+                        res.should.have.header('content-type', 'text/html; charset=utf-8');
+                        done();
+
+
+                    });
+            });
+        });
+    });
+
+});
