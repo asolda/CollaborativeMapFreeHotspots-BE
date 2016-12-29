66aa47e1151e6e498043c05c74d9189be5fca82f
matioski <mattia.rosselli@gmail.com>
2016-12-29 20:07:16 +0100

    Test sul file di selezione pin diretto sulle api

--- test/pin.js
+++ test/pin.js
@@ -4,9 +4,10 @@ let connection = require("../connection");
 //Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-
+let server = require("../app");
 let should = chai.should();
-
+connection.switch_db("test1");
+var expect = chai.expect;
 chai.use(chaiHttp);
 //Our parent block
 describe('Pins', () => {
@@ -15,19 +16,16 @@ describe('Pins', () => {
       * Test the /GET route
       */
     describe('/GET Pin', () => {
-        it('it should GET PINS list', (done) => {
+        it('it should GET PINS list', function (done) {
 
-            chai.request('http://localhost:8080')
+            chai.request(server)
                 .get('/pin/get_networks/1/2/3/4')
 
                 .end((err, res) => {
-                    res.should.have.status(200);
-
-                    res.should.have.header('content-type', 'text/html; charset=utf-8');
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.a('array');
                     done();
-
-
-
                 });
         });
     });
