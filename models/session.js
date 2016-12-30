47109eaa33df2623bc1a89226c4a8b67973e4935
Cesare Tucci <Cesare Tucci>
2016-12-31 00:05:53 +0100

    Aggiunta funzioni al session.js

--- models/session.js
+++ models/session.js
@@ -9,7 +9,7 @@ function Session(){
         var token=crypto.createHash('sha256').update(uuid.v1()).update(crypto.randomBytes(256)).digest("hex");//crea il token senza possibilità di collisioni     
         return new Promise((resolve,reject)=>{
             connection.acquire(function(err,con){
-                con.query('INSERT INTO sessione (session_id, userAgent, utente, utimo_accesso, indirizzo_ip) values(?,?,?,?,?)', [token, 'test', userId, (new Date().getTime()), ipaddress],function(err, result){
+                con.query('INSERT INTO sessione (session_id, user_agent, utente, utimo_accesso, indirizzo_ip) values(?,?,?,?,?)', [token, 'test', userId, (new Date().getTime()), ipaddress],function(err, result){
                     con.release();
                     resolve(token);
                     catch(err){
@@ -20,10 +20,25 @@ function Session(){
         })
     };
     
+   
+    this.drop=function(id){
+         return new Promise((resolve,reject)=>{
+            connection.acquire(function(err, con){
+                con.query('DELETE FROM sessione WHERE session_id= ?', [id], function(err, result){
+                    con.release();
+                    if(err){
+                        reject(err);
+                    }else{
+                        resolve(id);
+                    }
+                });
+            });
+        })
+    };
     
-    this.drop=function(userId){};
-    
-    this.updateTime=function(userId){};
+    this.updateTime=function(id){
+        //
+    };
     
 }
 
