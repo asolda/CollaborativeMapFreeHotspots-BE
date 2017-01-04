4793a2a0e1243bd4ea764daecad23f4690d36e8f
Mattia Rosselli <matioski@MBP-di-Mattia.station>
2017-01-04 13:42:39 +0100

    Aggiunto test user

---
+++ test/user.js
@@ -0,0 +1,43 @@
+
+
+let connection = require("../connection");
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require("../app");
+let should = chai.should();
+connection.switch_db("test1");
+let user = require("../models/user");
+var expect = chai.expect;
+chai.use(chaiHttp);
+//Our parent block
+describe('User', () => {
+    let userjson = { 'email': 'Test1@gmail.com', 'password': 'Cico1996' };
+    /*
+      * Testa la funzione di ricerca pin vicini
+      */
+    describe('Crea utente', () => {
+        it('Crea la richiesta per l\'utente Test1@gmail.com', function (done) {
+
+            chai.request(server)
+                .post('/user/new/request')
+                .send(userjson)
+
+                .end((err, res) => {
+                    expect(res.body).to.have.property('status', 0);
+
+                    expect(res.body).to.have.property('message', 'REGISTRATION_REQUEST_OK');
+                    done();
+                });
+        });
+        describe('Aggiunta di Test1@gmail.com al database.', (done) => {
+            it('testa la creazione vera e propria di un utente', function (done) {
+                user.create(userjson)
+                .then(message_ok =>{
+                    done();
+                });
+            });
+        });
+
+    });
+});
