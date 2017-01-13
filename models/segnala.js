e63f717b3df6295e3a1831689f15f4db4987747f
Finalgalaxy <finalgalaxy@gmail.com>
2017-01-13 20:24:26 +0100

    Fixata funzionalità di segnalazione, parametro update mancante

--- models/segnala.js
+++ models/segnala.js
@@ -4,18 +4,13 @@ var connection = require('../connection');
 function Segnala(){
 	this.report = function(user_id, req, res){
 		connection.acquire(function(err, con){
-            /*
-                INSERT INTO segnalazione (utente, rete_wifi, tipo, dettagli)
-                VALUES ('+req.body.utente+', '+req.body.rete_wifi+', '+req.body.tipo+', '+req.body.dettagli+')
-            */
-            
             con.query('INSERT INTO segnalazione (utente, rete_wifi, tipo, dettagli) VALUES (?, ?, ?, ?)', [user_id, req.body.rete_wifi, req.body.tipo, req.body.dettagli],
 				function(err, result) {
 					con.release();
                     if(err!=null) res.send({status: 1, message: 'ERROR_DB'});
                     else{
                         if(result.affectedRows>0){
-                            con.query('UPDATE rete_wifi SET numero_segnalazioni = numero_segnalazioni + 1 WHERE id = ?', function(err, result){
+                            con.query('UPDATE rete_wifi SET numero_segnalazioni = numero_segnalazioni + 1 WHERE id = ?', [req.body.rete_wifi], function(err, result){
                                 if(err){
                                     res.send({status: 1, message: 'ERROR_DB'});
                                 }else{
